8c43d1ccbd83fa0461ee19c22ae25a36a48d8164
Robert Hilton <robert.a.hilton.jr@gmail.com>
2021-12-17 22:44:48 +0000

    Update

--- imgui-config.cmake
+++ imgui-config.cmake
@@ -1,3 +1,3 @@
 cmake_minimum_required(VERSION 3.8)
 set(IMGUI_INCLUDE_DIRS "/usr/local/include/imgui")
-set(IMGUI_LIBRARIES "/usr/local/lib/imgui.a")
+set(IMGUI_LIBRARIES "/usr/local/lib/libimgui.a")
